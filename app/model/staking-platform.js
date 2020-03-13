99d2c025cd95c0e7069fb239ca785c50ae009262
trinhdn <trinhdn@blockchainlabs.asia>
2020-03-13 10:00:01 +0700

    update staking platform model

--- app/model/staking-platform.js
+++ app/model/staking-platform.js
@@ -1,4 +1,3 @@
-
 const timeUnit = require("./value-object/time-unit");
 const StakingType = require("./value-object/staking-type");
 const StakingPlatformStatus = require("./value-object/staking-platform-status");
@@ -10,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       allowNull: false,
       defaultValue: DataTypes.UUIDV4(),
+    },
+    platform: {
+      type: DataTypes.STRING(32),
+      allowNull: true
     },
     name: {
       type: DataTypes.STRING(256),
@@ -80,6 +83,18 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(128),
       allowNull: true
     },
+    erc20_duration: {
+      type: DataTypes.STRING(128),
+      allowNull: true
+    },
+    erc20_reward_estimate: {
+      type: DataTypes.STRING(128),
+      allowNull: true
+    },
+    erc20_validator_fee: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
     validator_address: {
       type: DataTypes.STRING(128),
       allowNull: true
@@ -87,21 +102,29 @@ module.exports = (sequelize, DataTypes) => {
     deleted_flg: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      default: false
+      defaultValue: false
     },
     created_by: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      default: 0
+      defaultValue: 0
     },
     updated_by: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      default: 0
-    }
+      defaultValue: 0
+    },
+    tx_id: {
+      type: DataTypes.STRING(256),
+      allowNull: true
+    },
+    wait_blockchain_confirm_status_flg: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
   }, {
       underscored: true,
       timestamps: true,
     });
 }
-
