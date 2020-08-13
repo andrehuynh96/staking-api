fe1e7999a6542dcafa676d34604d2cff8b45aa41
ducnt <ducnt@blockchainlabs.asia>
2020-08-13 13:25:16 +0700

    add check amount vote stake add

--- app/lib/platform-infor/infinito-api.js
+++ app/lib/platform-infor/infinito-api.js
@@ -13,7 +13,7 @@ module.exports = {
   getTransaction: async ({ platform, tx_id }) => {
     return await _getTransaction(platform, tx_id);
   },
-  getAmountOfTransaction: async ({ platform, tx_id }) => {
+  getAmountOfTransaction: async ({ platform, tx_id, address }) => {
     try {
       let amount = null;
       console.log('platform', platform.toLowerCase())
@@ -24,6 +24,9 @@ module.exports = {
         case 'atom':
           amount = await _getAmountOfTransactionATOM(platform, tx_id);
           break;
+        case 'ada': 
+          amount = await _getAmountOfTransactionADA(address);
+          break;
       }
       return amount;
     }catch (err) {
@@ -32,6 +35,19 @@ module.exports = {
     }
   }
 };
+
+async function _getAmountOfTransactionADA(address){
+  try {
+    const {data} = await api.ADA.getBalance(address);
+    if(data){
+      return BigNumber(data.balance).times(BigNumber(10).pow(-6)).toFixed();;
+    }
+    return null;
+  }catch (err) {
+    logger.error('infinito getAmountTransaction no found data with platform and tx_id' + platform + '/' + tx_id, err);
+    throw err;
+  }
+}
 
 async function _getAmountOfTransactionIRIS(platform, tx_id){
   try {
@@ -83,7 +99,6 @@ async function _getAmountOfTransactionATOM(platform, tx_id){
 
 async function _getTransaction (platform, tx_id){
   try {
-    console.log('opts', opts)
     const coinAPI= api.getChainService()[platform];
     const {data}=await coinAPI.getTransaction(tx_id);
     return data
