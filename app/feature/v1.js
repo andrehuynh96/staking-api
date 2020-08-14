7bb67d48cca2bb1e667bcd734ca7b1b57feccbb0
THINHNQ <thinhnq@blockchainlabs.asia>
2020-08-14 12:20:43 +0700

    change route

--- app/feature/v1.js
+++ app/feature/v1.js
@@ -19,6 +19,7 @@ router.use(require("./partner-request/partner-request.route"));
 // account contribute
 router.use(require("./account-contribution/account-contribution.route"))
 
-router.use(require("./validator/validator.route"))
+// validator
+router.use(require("./validator-info/validator-info.route"))
 
 module.exports = router;
